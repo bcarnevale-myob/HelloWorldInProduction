f4591041483fdefe7fe53074760bece8218e2e7e
bcarnevale-myob <bianca.carnevale@myob.com>
2019-04-30 10:11:57 +1000

    Date added to output string
    Current date and time have been added to the output string. Formatting
    needs to be improved.

--- src/com/company/RequestHandler.java
+++ src/com/company/RequestHandler.java
@@ -5,16 +5,33 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class RequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange request) throws IOException {
-        String response = "Hello Bianca";
+        String currentTime = currentTime();
+        String currentDate = currentDate();
+        String response = "Hello Bianca - the time on the server is "  + currentTime + " on " + currentDate;
         request.sendResponseHeaders(200, response.length());
         OutputStream outputStream = request.getResponseBody();
         outputStream.write(response.getBytes());
         outputStream.close();
+    }
+
+    public String currentTime() {
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        return timeFormat.format(date);
+    }
+
+    public String currentDate() {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        return dateFormat.format(date);
     }
 
 }
