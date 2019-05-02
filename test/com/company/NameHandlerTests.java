187f003f73ce4b7089489ffec23dd139010f646c
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-02 14:14:46 +1000

    Removed two tests from NameHandlerTests
    The tests were removed as they were duplicated in the
    WebApplicationTests

--- test/com/company/NameHandlerTests.java
+++ test/com/company/NameHandlerTests.java
@@ -15,39 +15,6 @@ import static org.mockito.Mockito.when;
 public class NameHandlerTests {
 
     @Test
-    public void userEndpointDoesNotAllowsGetRequests() throws IOException {
-        NameRepository names = new NameRepository();
-        NameHandler nameHandler = new NameHandler(names);
-
-        HttpExchange mockRequest = Mockito.mock(HttpExchange.class);
-
-        when(mockRequest.getRequestMethod()).thenReturn("GET");
-
-        nameHandler.handle(mockRequest);
-
-        verify(mockRequest).sendResponseHeaders(404, 0);
-        verify(mockRequest).close();
-
-    }
-
-    @Test
-    public void userEndpointPostRequestsRespondsWithA201Response() throws IOException, URISyntaxException {
-        NameRepository names = new NameRepository();
-        NameHandler nameHandler = new NameHandler(names);
-
-        HttpExchange mockRequest = Mockito.mock(HttpExchange.class);
-
-        when(mockRequest.getRequestMethod()).thenReturn("POST");
-        when(mockRequest.getRequestURI()).thenReturn(new URI("/users/bianca"));
-
-        nameHandler.handle(mockRequest);
-
-        verify(mockRequest).sendResponseHeaders(201, 0);
-        verify(mockRequest).close();
-
-    }
-
-    @Test
     public void PostRequestsAddsNameToTheNameRepository() throws IOException, URISyntaxException {
         NameRepository names = new NameRepository();
 
