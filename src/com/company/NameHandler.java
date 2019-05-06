719d84d40cdf8d54559cfe7dca4e294b7ddc1755
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-06 14:12:08 +1000

    Can remove a name from the name repository
    A name can be removed from the name repository through the delete method
    under the nameHandler. A remove() method needed to be implemented for
    the name repository.

--- src/com/company/NameHandler.java
+++ src/com/company/NameHandler.java
@@ -15,6 +15,8 @@ public class NameHandler extends HelloWorldHandler {
         switch(request.getRequestMethod()){
             case "POST":
                 postHandler(request);
+            case "DELETE":
+                deleteHandler(request);
             default:
                 notFound(request);
         }
@@ -32,6 +34,19 @@ public class NameHandler extends HelloWorldHandler {
         String nameFormatted = nameFromPath.substring(0,1).toUpperCase() + nameFromPath.substring(1).toLowerCase();
 
         names.add(nameFormatted);
+    }
+
+    private void deleteHandler(HttpExchange request) throws IOException {
+        request.sendResponseHeaders(200, 0);
+
+        String path = request.getRequestURI().getPath();
+
+        String[] pathTokens = path.split("/");
+
+        String nameFromPath = pathTokens[pathTokens.length - 1];
+        String nameFormatted = nameFromPath.substring(0,1).toUpperCase() + nameFromPath.substring(1).toLowerCase();
+
+        names.remove(nameFormatted);
     }
 
     private void notFound(HttpExchange request) throws IOException {
--- test/com/company/NameHandlerTests.java
+++ test/com/company/NameHandlerTests.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -62,6 +62,35 @@ public class NameHandlerTests {
 
         assertTrue(names.get().contains("Bianca"));
         assertTrue(names.get().contains("Fiona"));
+
+    }
+
+    @Test
+    public void DeleteRequestsRemovesANameFromTheNameRepository() throws IOException, URISyntaxException {
+        NameRepository names = new InMemoryNameRepository();
+
+        NameHandler userHandler = new NameHandler(names);
+
+        HttpExchange mockRequest1 = Mockito.mock(HttpExchange.class);
+
+        when(mockRequest1.getRequestMethod()).thenReturn("POST");
+        when(mockRequest1.getRequestURI()).thenReturn(new URI("/names/bianca"));
+        userHandler.handle(mockRequest1);
+
+        verify(mockRequest1).sendResponseHeaders(201, 0);
+        verify(mockRequest1).close();
+
+        HttpExchange mockRequest2 = Mockito.mock(HttpExchange.class);
+
+        when(mockRequest2.getRequestMethod()).thenReturn("DELETE");
+        when(mockRequest2.getRequestURI()).thenReturn(new URI("/names/bianca"));
+
+        userHandler.handle(mockRequest2);
+
+        verify(mockRequest2).sendResponseHeaders(200, 0);
+        verify(mockRequest2).close();
+
+        assertFalse(names.get().contains("Bianca"));
 
     }
 
