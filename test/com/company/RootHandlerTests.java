698ac0e88a8f281544f71748d1a10146e0ba7c37
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-06 13:55:03 +1000

    Response is formatted correctly
    The response is formatted with ',' and 'and' in the correct position
    when adding names to the response.

--- test/com/company/RootHandlerTests.java
+++ test/com/company/RootHandlerTests.java
@@ -44,6 +44,48 @@ public class RootHandlerTests {
 
     }
 
+    @Test
+    public void TwoNamesCanBeAddedToTheResponse() throws IOException {
+        HelloWorldServer helloWorldServer = new HelloWorldServer(8080);
+
+        helloWorldServer.createServer();
+
+        URL urlPost = new URL("http://localhost:8080/names/fiona");
+        HttpURLConnection connection = (HttpURLConnection) urlPost.openConnection();
+        connection.setRequestMethod("POST");
+
+        int responseCode = connection.getResponseCode();
+        connection.disconnect();
+
+        assertEquals(201, responseCode);
+
+        URL urlPost2 = new URL("http://localhost:8080/names/renae");
+        HttpURLConnection connection2 = (HttpURLConnection) urlPost2.openConnection();
+        connection2.setRequestMethod("POST");
+
+        int responseCode2 = connection2.getResponseCode();
+        connection2.disconnect();
+
+        assertEquals(201, responseCode2);
+
+        URL urlGet = new URL("http://localhost:8080/");
+        HttpURLConnection connection3 = (HttpURLConnection) urlGet.openConnection();
+        connection3.setRequestMethod("GET");
+
+        int responseCode3 = connection3.getResponseCode();
+
+        var bytes = connection3.getInputStream().readAllBytes();
+        var response = new String(bytes);
+
+        var expectedRegex = Pattern.compile("Hello Bianca, Fiona and Renae - the time on the server is [0-9]{1,2}:[0-9]{2}(am|pm) on [0-3]?[0-9] \\w+ [0-9]{4}");
+
+        var responseMatches = expectedRegex.matcher(response).matches();
+        assertEquals(200, responseCode3 );
+
+        assertTrue(responseMatches, "\n" + response+ "\n" + expectedRegex.toString());
+
+    }
+
 }
 // 2 tests: name added to response and name can be read from response
 // string compare to the regex
