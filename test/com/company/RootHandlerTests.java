81fcdcbd76e59b503d7b6cf1402dfc141dfea985
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-08 13:38:37 +1000

    Update names of values in tests
    The names of values in the RootHandler have been updated for better
    readibility.

--- test/com/company/RootHandlerTests.java
+++ test/com/company/RootHandlerTests.java
@@ -38,8 +38,8 @@ public class RootHandlerTests {
 
         assertEquals(201, responseCode);
 
-        URL urlPost2 = new URL("http://localhost:8080/");
-        HttpURLConnection connection2 = (HttpURLConnection) urlPost2.openConnection();
+        URL urlGet = new URL("http://localhost:8080/");
+        HttpURLConnection connection2 = (HttpURLConnection) urlGet.openConnection();
         connection2.setRequestMethod("GET");
 
         int responseCode2 = connection2.getResponseCode();
@@ -117,27 +117,27 @@ public class RootHandlerTests {
         assertEquals(201, responseCode2);
 
         URL urlDelete = new URL("http://localhost:8080/names/renae");
-        HttpURLConnection connection4 = (HttpURLConnection) urlDelete.openConnection();
-        connection4.setRequestMethod("DELETE");
-
-        int responseCode4 = connection4.getResponseCode();
-        connection4.disconnect();
-
-        assertEquals(202, responseCode4);
-
-        URL urlGet = new URL("http://localhost:8080/");
-        HttpURLConnection connection3 = (HttpURLConnection) urlGet.openConnection();
-        connection3.setRequestMethod("GET");
+        HttpURLConnection connection3 = (HttpURLConnection) urlDelete.openConnection();
+        connection3.setRequestMethod("DELETE");
 
         int responseCode3 = connection3.getResponseCode();
+        connection3.disconnect();
 
-        var bytes = connection3.getInputStream().readAllBytes();
+        assertEquals(202, responseCode3);
+
+        URL urlGet = new URL("http://localhost:8080/");
+        HttpURLConnection connection4 = (HttpURLConnection) urlGet.openConnection();
+        connection4.setRequestMethod("GET");
+
+        int responseCode4 = connection4.getResponseCode();
+
+        var bytes = connection4.getInputStream().readAllBytes();
         var response = new String(bytes);
 
         var expectedRegex = Pattern.compile("Hello Bianca and Fiona - the time on the server is [0-9]{1,2}:[0-9]{2}(am|pm) on [0-3]?[0-9] \\w+ [0-9]{4}");
 
         var responseMatches = expectedRegex.matcher(response).matches();
-        assertEquals(200, responseCode3 );
+        assertEquals(200, responseCode4 );
 
         assertTrue(responseMatches, "\n" + response+ "\n" + expectedRegex.toString());
 
