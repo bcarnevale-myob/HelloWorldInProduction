5e8ff55f30ebb79b267388c9e954cfefacbdd913
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-08 11:30:54 +1000

    Acceptance test added for added names
    An acceptance test is added to send a post request to /names/fiona and
    then a get request to /.

--- test/com/company/AcceptanceTest.java
+++ test/com/company/AcceptanceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
@@ -46,6 +47,35 @@ class AcceptanceTest {
         assertEquals(200, responseCode);
         assertTrue(responseMatches, "\n" + response+ "\n" + expectedRegex.toString());
 
+    }
+
+    @Test
+    public void helloWorldAcceptanceTestWithAddedNames() throws IOException {
+        URL urlPost = new URL("http://localhost:8080/names/fiona");
+        HttpURLConnection connectionPost = (HttpURLConnection) urlPost.openConnection();
+        connectionPost.setRequestMethod("POST");
+
+        int responseCodePost = connectionPost.getResponseCode();
+        connectionPost.disconnect();
+
+        assertEquals(201, responseCodePost);
+
+        URL url = new URL("http://localhost:8080/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+
+        var bytes = connection.getInputStream().readAllBytes();
+        var response = new String(bytes);
+
+        var expectedRegex = Pattern.compile("Hello Bianca and Fiona - the time on the server is [0-9]{1,2}:[0-9]{2}(am|pm) on [0-3]?[0-9] \\w+ [0-9]{4}");
+
+        var responseMatches = expectedRegex.matcher(response).matches();
+
+        assertEquals(200, responseCode);
+        assertTrue(responseMatches, "\n" + response+ "\n" + expectedRegex.toString());
     }
 
 }
