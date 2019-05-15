fc46fad343002722db09924e31c7136b5992753e
bcarnevale-myob <bianca.carnevale@myob.com>
2019-05-15 13:57:01 +1000

    Fix master

--- src/com/company/NameRepository.java
+++ src/com/company/NameRepository.java
@@ -3,7 +3,6 @@ package com.company;
 import java.util.Collection;
 
 public interface NameRepository extends Collection<String> {
-
     boolean add(String name);
 
     boolean remove(String name);
